f168ff80979ead69e1f3be58d6feb8d51b22b8cb
rptshri <rptshrivastava@gmai.com>
2019-11-20 15:49:46 +0530

    update for code

--- Codes/Flesx_test/Flesx_test.ino
+++ Codes/Flesx_test/Flesx_test.ino
@@ -1,4 +1,5 @@
-#define DEBUG true
+//#define DEBUG true
+#define plotCSV
 
 int flexiForcePin = A0; //analog pin 0
 float flexiForceReading = 0;
@@ -21,30 +22,42 @@ void loop()
   for (int i = 0; i < 10; i++)
   {
     flexiForceReading = analogRead(flexiForcePin);
-    Voltage_Reading = (flexiForceReading / 1024) * 5000;
-    Weight_lbs = mapfloat(Voltage_Reading, 4400, 0, 100, 0);
+    Voltage_Reading = (flexiForceReading / 1024) * 4777;
+    if (Voltage_Reading <= 3600)
+    {
+      Weight_lbs = mapfloat(Voltage_Reading, 3600, 0, 20, 0);
+    }
+    else
+      Weight_lbs = mapfloat(Voltage_Reading, 4400, 3600, 100, 20);
+    buf[i] = Voltage_Reading;
     buf[i] = Weight_lbs;
     avgValue += buf[i];
-#ifdef DEBUG
-    Serial.print(Voltage_Reading);
-    Serial.print("\t");
-    Serial.print(buf[i]);
-    Serial.println("\t");
-#endif
-    delay(20);
+    //#ifdef DEBUG
+    //    Serial.print(Voltage_Reading);
+    //    Serial.print("\t");
+    //    Serial.print(buf[i]);
+    //    Serial.println("\t");
+    //#endif
+    delay(10);
   }
   final_Weight_lbs = (avgValue / 10);
   final_Weight_kgs = final_Weight_lbs * 0.45359237;
 
-#ifdef DEBUG
-  Serial.print("\n");
-  Serial.print(flexiForceReading);
-  Serial.print("\t");
+  //#ifdef DEBUG
+  //  Serial.print("\n");
+  //  Serial.print(final_Weight_lbs);
+  //  Serial.print("\t");
+  //  Serial.println(final_Weight_kgs);
+  //  Serial.print("\n");
+  //#endif
+
+#ifdef plotCSV
   Serial.print(final_Weight_lbs);
-  Serial.print("\t");
+  Serial.print(",");
   Serial.println(final_Weight_kgs);
 #endif
-  //  delay(250); //just here to slow down the output for easier reading
+
+  delay(0); //just here to slow down the output for easier reading
 }
 
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
